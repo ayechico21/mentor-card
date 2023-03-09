f2fa9ca4fd05a39c2764eb1068260d68375a5dbf
Maneesh Cheema <ayechico21@gmail.com>
2023-03-09 21:52:57 +0530

    added stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import CornerCircle from "./components/CornerCircle/CornerCircle";
 import MentorInfo from "./components/MentorInfo/MentorInfo";
+import Star from "./components/Star/Star";
 function App() {
   return (
     <div className="App">
--- src/components/MentorInfo/MentorInfo.jsx
+++ src/components/MentorInfo/MentorInfo.jsx
@@ -12,14 +12,14 @@ function MentorInfo() {
   return (
     <MentorInfoWrapper>
       <div className="rating">{val.toFixed(1)}</div>
-      <div className="stars">Stars</div>
+      <div className="stars"></div>
       <h1>Mentor Name</h1>
       <h2>Mentor Company</h2>
       <p>
-        A curious learner, on a way to become a kickass developer who writes
-        Python, Java, Javascript and Go | Software Engineer @Groww | Ex-Software
-        Engineer @Grofers | Tech Consultant | Freelancer | Open Source Developer
-        | Coding Instructor
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ad dolores
+        autem mollitia! Mollitia, ipsam! Odit, eos blanditiis. Dolorem optio hic
+        totam voluptatum distinctio quibusdam voluptatibus officia laudantium
+        quis aut.
       </p>
     </MentorInfoWrapper>
   );
